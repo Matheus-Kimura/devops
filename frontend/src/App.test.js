1bfbaf3e6115ab2ea06dba30acde787730bfed75
Matheus Kimura <matheus.kimura@inspira.com.br>
2024-04-10 15:29:26 -0300

    Creates github workflow to frontend

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App component', () => {
+  it('updates state when input values change', () => {
+    const { container } = render(<App />);
+    const inputFields = container.querySelectorAll('.input-field');
+
+    const input1 = inputFields[0];
+    const input2 = inputFields[1];
+
+    fireEvent.change(input1, { target: { value: '5' } });
+    fireEvent.change(input2, { target: { value: '10' } });
+
+    expect(input1.value).toBe('5');
+    expect(input2.value).toBe('10');
+  
+    expect(input1.value).toBe('5');
+    expect(input2.value).toBe('10');
+  });
+})
